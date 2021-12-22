f255aa9a480ec974bdfdd027ab1dd875e4c429da
jiinahn92@gmail.com <jiinahn92@gmail.com>
2021-12-22 17:15:18 +0900

    first commit

---
+++ src/main/java/com/nanoom/nanoom/NanoomApplication.java
@@ -0,0 +1,13 @@
+package com.nanoom.nanoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nanoom/nanoom/NanoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanoom.nanoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
